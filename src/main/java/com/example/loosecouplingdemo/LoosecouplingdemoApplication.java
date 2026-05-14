2454ea49b140bb48cac7d02b7ade7e3154d077ff
Aniket Dave <daveaniket002@gmail.com>
2026-05-14 08:49:07 +0530

    setup

---
+++ src/main/java/com/example/loosecouplingdemo/LoosecouplingdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.loosecouplingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoosecouplingdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoosecouplingdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/loosecouplingdemo/LoosecouplingdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loosecouplingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoosecouplingdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
